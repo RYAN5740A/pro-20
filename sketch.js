7b6069ecda8da26355604ffe339b4c7ae3853ef8
RYAN5740A <81171121+RYAN5740A@users.noreply.github.com>
2021-04-19 11:39:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var cat1
+var tom2
+var tom3
+var tom4
+var jerry1
+var jerry2
+var jerry3
+var jerry4
+function preload() {
+    // //load the images here
+    gardenImg=loadImage("garden.png")
+}
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+    garden=createSprite(500,400)
+   garden.addImage("gardenImg")
+tom=createSprite(600,500,50,50)
+
+jerry=createSprite(200,500,50,50)
+
+
+}
+
+function draw() {
+
+    background(0);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
